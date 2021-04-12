d6422db7e91fc94e7dd6749a8bd5367deba66b94
Damyn Filipuzzi <damynfilipuzzi@gmail.com>
2021-04-11 19:35:01 -0700

    Update post.component.js

--- code/react-src/src/components/post.component.js
+++ code/react-src/src/components/post.component.js
@@ -1,1 +1,10 @@
-//TODO
+import React, {useEffect} from 'react'
+import styled from '@emotion/styled';
+
+import { useStoreActions, useStoreState } from 'easy-peasy';
+
+function Posts() {
+    
+}
+
+export default Posts;
